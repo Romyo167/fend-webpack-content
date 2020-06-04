820e54ba3378370bdf58de5342b13426e503badf
U-DESKTOP-DERKF97\Ramy Nosir <nosirramy97@gmail.com>
2020-06-04 20:59:22 +0200

    test loader

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    // ...
+    entry : './src/client/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader',
+          options: {
+            // eslint options (if necessary)
+          },
+        },
+      ],
+    },
+    // ...
+  };
